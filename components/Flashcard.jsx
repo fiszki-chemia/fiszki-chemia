63794da06053abc2e48c8590971e8aef8a19bedf
Kemt66 <112822919+Kemt66@users.noreply.github.com>
2025-09-03 21:04:46 +0200

    Delete components directory

--- components/Flashcard.jsx
+++
@@ -1,8 +0,0 @@
-export default function Flashcard({ card }) {
-  return (
-    <div className="p-4 my-2 bg-gray-100 rounded shadow">
-      <p className="font-semibold">{card.question}</p>
-      <p className="text-gray-500">{card.answer}</p>
-    </div>
-  )
-}
