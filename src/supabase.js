103eb73c9f4baec74825fda6a4d46857227eb3aa
SlothsHaveFeelings <87302811+kajtekor@users.noreply.github.com>
2025-09-03 20:57:58 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://ehfnsrzbtcrgnrueykey.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZm5zcnpidGNyZ25ydWV5a2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MjU0MjUsImV4cCI6MjA3MjUwMTQyNX0.D4gOXVwgr_X8dNAIZ6bfnCDBfKiNO-t9KU55huQYfPA'
+export const supabase = createClient(supabaseUrl, supabaseKey)
