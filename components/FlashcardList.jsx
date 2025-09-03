4649c681e50954e147dc04765a282eb5f21849ed
Kemt66 <112822919+Kemt66@users.noreply.github.com>
2025-09-03 21:00:41 +0200

    Update FlashcardList.jsx

--- components/FlashcardList.jsx
+++ components/FlashcardList.jsx
@@ -1,1 +1,23 @@
+import { useEffect, useState } from 'react'
+import { supabase } from '../supabase'
+import Flashcard from './Flashcard'
 
+export default function FlashcardList() {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      let { data, error } = await supabase.from('flashcards').select('*')
+      if (!error) setCards(data)
+    }
+    fetchCards()
+  }, [])
+
+  return (
+    <div>
+      {cards.map((card) => (
+        <Flashcard key={card.id} card={card} />
+      ))}
+    </div>
+  )
+}
