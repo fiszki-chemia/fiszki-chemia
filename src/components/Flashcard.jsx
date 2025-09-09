160776038126aa548655a00e31e28265c45060ea
SlothsHaveFeelings <87302811+kajtekor@users.noreply.github.com>
2025-09-09 18:38:55 +0200

    Update Flashcard.jsx

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -10,6 +10,7 @@ export default function Flashcard({ question, answer, flipped }) {
       }}
     >
       <div
+        className = "flashcard-inner"
         style={{
           width: '100%',
           height: '100%',
