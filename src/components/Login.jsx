ecf49f68c4f51bd28a5516e11478bae0c4fa60a4
SlothsHaveFeelings <87302811+kajtekor@users.noreply.github.com>
2025-09-20 15:40:07 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -99,8 +99,6 @@ export default function Login({ initialMessage }) {
           style={{
             border: '1px solid #ccc',
             padding: '10px',
-            marginRight: '5px',
-            marginLeft: '10px',
             marginBottom: '16px',
             width: '100%',
             borderRadius: '4px',
