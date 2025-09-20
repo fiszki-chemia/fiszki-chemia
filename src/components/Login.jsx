8d790c17751bf7b22c7c7e4244d8ec90f4141ea1
SlothsHaveFeelings <87302811+kajtekor@users.noreply.github.com>
2025-09-20 15:55:36 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -86,7 +86,7 @@ export default function Login({ initialMessage }) {
             border: '1px solid #ccc',
             padding: '10px',
             marginBottom: '12px',
-            width: '380px',
+            width: '379.5px',
             borderRadius: '4px',
           }}
         />
@@ -100,7 +100,7 @@ export default function Login({ initialMessage }) {
             border: '1px solid #ccc',
             padding: '10px',
             marginBottom: '16px',
-            width: '380px',
+            width: '379.5px',
             borderRadius: '4px',
           }}
         />
